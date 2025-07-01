4e493dc3b6f7932d6d8659e0e6701dfaf60c7ac6
TheUmbralPyre <TheUmbralPyre@gmail.com>
2025-07-01 10:37:00 +0300

    Finish Hamburger

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 console.log("Scripts Work")
+
+const hamburger = document.querySelector(".hamburger");
+const offmenu = document.querySelector(".off-menu");
+
+console.log(offmenu);
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    offmenu.classList.toggle("active");
+})
