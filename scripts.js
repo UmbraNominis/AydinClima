a84bb92ebafd90d86253703dbbb0bdf333d25805
TheUmbralPyre <TheUmbralPyre@gmail.com>
2025-07-02 09:53:52 +0300

    Add Fixed Nav after Certain Point

--- scripts.js
+++ scripts.js
@@ -5,8 +5,20 @@ const offmenu = document.querySelector(".off-menu");
 
 console.log(offmenu);
 
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     offmenu.classList.toggle("active");
-})
+});
+
+const nav = document.querySelector(".nav");
+console.log(nav);
+
+const watch = document.querySelector(".hero-button");
+
+function observerCallback(payload){
+    console.log(payload[0].isIntersecting);
+    nav.classList.toggle("sticky", !payload[0].isIntersecting);
+}
+
+const observer = new IntersectionObserver(observerCallback);
+observer.observe(watch);
