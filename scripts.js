8c2fa7f6307bc43fb8626cf94364aa908a5798c3
TheUmbralPyre <TheUmbralPyre@gmail.com>
2025-07-14 20:42:22 +0300

    Add order modal

--- scripts.js
+++ scripts.js
@@ -69,10 +69,31 @@ acs.forEach(ac => {
             <p class="ac-price-sub">+ Безплатен Монтаж</p>
 
             <div class="ac-buttons">
-                <button class="ac-button">
-                    <svg class="ac-button-svg" xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 16 16"><path anc="" fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"></path></svg>
-                        Направи Обаждане
-                </button>
+                <button class="ac-button"><svg class="ac-button-svg" xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 16 16"><path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>Направи Поръчка</button>
             </div>`;
+    card.querySelector(".ac-button").addEventListener("click", () => {
+        modal.showModal();
+    });
     acBestSelling.appendChild(card);
+});
+
+
+const modal = document.querySelector("#order-modal");
+const openButton = document.querySelector("#open-button");
+const closeButton = document.querySelector("#close-button");
+
+modal.addEventListener("click", e => {
+  const modalDimensions = modal.getBoundingClientRect()
+  if (
+    e.clientX < modalDimensions.left ||
+    e.clientX > modalDimensions.right ||
+    e.clientY < modalDimensions.top ||
+    e.clientY > modalDimensions.bottom
+  ) {
+    modal.close()
+  }
+});
+
+closeButton.addEventListener("click", () => {
+    modal.close();
 });
