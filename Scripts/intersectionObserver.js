4dfe5c0f1d05cb16be6ef91765296d95f6862dac
TheUmbralPyre <TheUmbralPyre@gmail.com>
2025-07-19 18:54:44 +0300

    Improve theme color switching

--- Scripts/intersectionObserver.js
+++ Scripts/intersectionObserver.js
@@ -1,12 +1,20 @@
 const nav = document.querySelector(".nav");
-
 const watch = document.createElement("div"); // Create a Div that Will be Used to Tell when the Navbar went Offscreen
 watch.setAttribute("js-scroll-watcher", ""); // Set an Attribute for Clarity
 nav.before(watch); // Place the Div Before the Navbar
+
+const onHomePage = window.location.pathname === "/index.html";
+
+const themeColor = document.querySelector("meta[name=theme-color]");
 
 function observerCallback(payload){
     nav.classList.toggle("sticky", !payload[0].isIntersecting); // Toggle the sticky class of the Navbar depending on wether the Scroll Div is Visible
 }
 
-const observer = new IntersectionObserver(observerCallback);
+function observerCallbackWithDynamicThemeColor(payload){
+    nav.classList.toggle("sticky", !payload[0].isIntersecting); // Toggle the sticky class of the Navbar depending on wether the Scroll Div is Visible
+    themeColor.setAttribute("content", !payload[0].isIntersecting ? "white" : "#253a64");
+}
+
+const observer = new IntersectionObserver(onHomePage ? observerCallbackWithDynamicThemeColor : observerCallback);
 observer.observe(watch);
